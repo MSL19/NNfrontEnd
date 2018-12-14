a61f6869ae88b9c82982b7cf35fad1d17d337192
MSL19 <40368820+MSL19@users.noreply.github.com>
2018-12-13 21:06:47 -0700

    now adds the RAW hidden and OUTput nodes to bigString
    this is impeative for the whole disply situation.....

--- testNode.js
+++ testNode.js
@@ -3,40 +3,88 @@ const proxyurl = "https://cors-anywhere.herokuapp.com/";
 const url = "https://www.kdsatp.org/nnpp/"; // site that doesn’t send Access-Control-*
 fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
 .then(response => response.text())
-.then(contents => 
+.then(contents => //if you have a function inside a function, you this to  
     console.log(JSON.parse(contents))
   //  mainData = JSON.parse(contents),
    // console.log(mainData)
 )
 .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
 console.log(mainData);*/
-var xmlhttp = new XMLHttpRequest();
-var mainJSONData;
-var url = "https://cors-anywhere.herokuapp.com/https://www.kdsatp.org/nnpp/";
-function setup(){
-  createCanvas(600,300);
+let data;
+let ready = false;
+async function setup(){
+  createCanvas(1000,400);
+
+  background(50);
 }
-function draw(){
-  fill(0);
-  ellipse(150,250,60);
+function overCircle(x,y,diameter) {
+  let disX = x - mouseX;
+  let disY = y - mouseY;
+  if(sqrt(sq(disX) + sq(disY)) < diameter/2 ) {
+    return true;
+  } else {
+    return false;
+  }
 }
-xmlhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    var nnData = JSON.parse(this.responseText);
-   // myFunction(myArr);
-   console.log(nnData);
-   var BH = nnData["BH"]["data"];
-   var BO = nnData["BO"]["data"];
-   var HOW = nnData["HOW"]["data"];
-   var IHW = nnData["IHW"]["data"];
-   console.log(BO);
-   console.log(IHW);
- //  mainJSONData = nnData;
-      
+
+async function draw(){
+  if(ready) {
+    textSize(15);
+    textAlign(CENTER, CENTER);
+
+  var i1 = data["inputs"][0];
+  var i2 = data["inputs"][1];
+  var i3 = data["inputs"][2];
+  fill(0,0,i1*255);
+  ellipse(100,100,60);
+  if(overCircle(100,100,60)){
+    fill(225);
+  }
+  text(i1.toString().substring(0,5), 100,100);
+  fill(0,0,i2*255);
+  ellipse(100,200,60);
+
+  if(overCircle(100,200,60)){
+    fill(225);
   }
   
-};
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
+  text(i2.toString().substring(0,5), 100,200);
+  fill(0,0,i3*255);
+  ellipse(100,300,60);
 
-//console.log(mainJSONData);
+  if(overCircle(100,300,60)){
+    fill(225);
+  }
+  text(i3.toString().substring(0,5), 100,300);
+  }
+
+  var i1 = data["inputs"][0];
+  var i2 = data["inputs"][1];
+  var i3 = data["inputs"][2];
+  ellipse(300,100,60);
+  ellipse(300,200,60);
+  ellipse(300,300,60);
+
+  ellipse(500,150,60);
+  ellipse(500,250,60);
+
+
+
+}
+
+async function getData() {
+  let req = await fetch('https://kdsatp.org/nnpp/');
+  let data = await req.json();
+  console.log(data);
+  ready = true;
+  return data;
+}
+async function setData(){
+  data = await getData();
+}
+setInterval(setData, 4000);
+/*async function onLoad() {
+  let data = await getData();
+}
+*/
+//window.onload = onLoad;
